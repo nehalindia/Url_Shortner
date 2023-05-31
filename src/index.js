ffea89dfddcd102081ca5085d3d139c6e0c866d3
Ravi <rraj34361@gmail.com>
2023-05-31 17:16:39 +0530

    defined strcture

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ const app = express()
 const port = 3000
 const route = require('./route/route')
 const bodyparser = require('body-parser')
+const {default : mongoose } = require('mongoose')
 
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://rraj34361:bXgwmkpBz9CHdAfr@cluster0.brjrlou.mongodb.net/Ravi34361", {
+    useNewUrlParser :true
+}).then(()=>{
+    console.log("mongodb is connected")
+}).catch((err)=>{
+    console.log(err)
+})
 
 
 
