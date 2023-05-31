1ed49e964d5d6a6e877d367e52beba3e9c9c730a
kazmy <nehaluddin.dpe@gmail.com>
2023-05-31 21:24:24 +0530

    Added api

---
+++ src/controller/urlController.js
@@ -0,0 +1,15 @@
+const urlModel = require('../models/urlModel')
+const validUrl = require('valid-url')
+const shortid = require('shortid');
+ 
+
+const createShortUrl = async function(req,res){
+    
+}
+// console.log(shortid.generate());
+  
+//     if (validUrl.isUri(suspect)){
+//         console.log('Looks like an URI');
+//     } else {
+//         console.log('Not a URI');
+//     }
--- src/route/route.js
+++ src/route/route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const {} = require('../controller/urlController')
 
 router.post('/url/shorten')
 router.get('/:urlCode')
