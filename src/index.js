025e6a10cbc565c198bd012ec22fc56d1342759c
Sufiyan Sayyed <sayyedsufiyan1301@gmail.com>
2023-06-01 15:01:43 +0530

    "Added query string in controller part line 37"

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ const  express = require('express')
 const app = express()
 const port = 3000
 const route = require('./route/route')
-const dotenv = require('dotenv')
 const {default : mongoose } = require('mongoose')
-dotenv.config()
 
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://nehaluddindpe:RCGtWC3HqBQUfNeR@cluster0.wzbtyg0.mongodb.net/blogingsite?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://nehaluddindpe:RCGtWC3HqBQUfNeR@cluster0.wzbtyg0.mongodb.net/urlshortner?retryWrites=true&w=majority", {
     useNewUrlParser :true
 }).then(()=>{
     console.log("mongodb is connected")
@@ -20,4 +18,4 @@ app.use('/',route)
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(30000, () => console.log(`App is listening on port `)) 
+app.listen(port, () => console.log(`App is listening on port ${port}`)) 
