cf41678bb807ff54a03d9fbc03833e793ec7f5df
kazmy <nehaluddin.dpe@gmail.com>
2023-06-01 15:09:27 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -19,6 +19,3 @@ app.use('/',route)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`App is listening on port ${port}`)) 
-
- app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`App is listening on port `)) 
