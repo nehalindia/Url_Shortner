07d2ab3fd3701d1449539a0a957d5d68af647f79
kazmy <nehaluddin.dpe@gmail.com>
2023-06-01 11:38:59 +0530

    cacheing added

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -17,13 +17,14 @@ const createShortUrl = async function(req,res){
         url = url.toLowerCase()
         let check = await urlModel.findOne({longUrl : url}).select({urlCode:1, shortUrl:1, longUrl:1, _id:0})
         if(check){
+            cache.set(check.urlCode, url, 60*60*60*60)
             return res.status(200).send({status:true, data:check })
         }
         else{
             let code = shortid.generate().toLowerCase()
             let data = {longUrl : url, urlCode: code, shortUrl: "http://localhost:3000/"+code }
             let result = await urlModel.create(data)
-            cache.set("my", "nehal", 1200000000)
+            cache.set(code, data.longUrl, 60*60*60*60)
             let my = {longUrl: result.longUrl, shortUrl: result.shortUrl, urlCode: result.urlCode}
             return res.status(200).send({status:true, data:my})
         }
@@ -41,23 +42,26 @@ const getUrl = async function(req,res){
             console.log(req.params.urlCode)
             return res.status(400).send({status :false, message: "Not a valid Url"})
         }
-        let ab= "my"
-        if (cache.has(ab)) {
-            // Key exists in the cache
+        let code = req.params.urlCode
+        if (cache.has(code)) {
             console.log("cache exist")
         }
         else{
             console.log("cache not exist")
         }
           
-        const checkCache = cache.get(ab);
+        const checkCache = cache.get(code);
         console.log(checkCache)
-        return res.status(200).send({status :true, message: checkCache})
+        if(checkCache){
+            return res.status(302).redirect(checkCache);
+        }
+        //return res.status(200).send({status :true, message: checkCache})
         let data = await urlModel.findOne({urlCode: req.params.urlCode}).select({urlCode:1, shortUrl:1, longUrl:1, _id:0})
         if(!data){
             return res.status(403).send({status :false, message: "Not a valid Url"})
+        }else{
+            res.status(302).redirect(data.longUrl);
         }
-        res.status(302).redirect(data.longUrl);
     }catch(error){
         res.status(500).send({status : false,message: error.message})
     }
