badf780025f5a5da42690a6f402a1198711a764c
kazmy <nehaluddin.dpe@gmail.com>
2023-06-02 17:33:25 +0530

    changes in redis

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -14,16 +14,16 @@ const redisClient = redis.createClient(
     16476,
     "redis-16476.c212.ap-south-1-1.ec2.cloud.redislabs.com",
     { no_ready_check: true }
-  );
-  redisClient.auth("lW4mvHKbk3LHvaX3u5LxICpwGtVawdI1", function (err) {
+);
+redisClient.auth("lW4mvHKbk3LHvaX3u5LxICpwGtVawdI1", function (err) {
     if (err) {
         console.log(err.message)
     }  // throw err;
-  });
+});
   
-  redisClient.on("connect", async function () {
+redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
-  });
+});
 
   
   
@@ -38,7 +38,7 @@ const createShortUrl = async function(req,res){
     try{
         let protocol = req.protocol;
         // let host = req.hostname;
-        console.log(req.rawHeaders)
+        // console.log(req.rawHeaders)
         let rawHeaders = req.rawHeaders
         let hostName
         for(let i = 0; i<rawHeaders.length; i++){
@@ -64,10 +64,10 @@ const createShortUrl = async function(req,res){
     //   }
         let check = await urlModel.findOne({longUrl : url}).select({urlCode:1, shortUrl:1, longUrl:1, _id:0})
         if(check){
-            cache.set(check.urlCode, url)
+            // cache.set(check.urlCode, url)
            // return res.status(200).send({status:true, data:check })
             // cache.set(check.urlCode, url, 60*60*60*60)
-            await SET_ASYNC(`${check.urlCode}`, JSON.stringify(check.longUrl))      ///dekh lenge stringify use krna hai ya nhi
+            await SET_ASYNC(`${check.urlCode}`, check.longUrl)      ///dekh lenge stringify use krna hai ya nhi
             return res.status(200).send({status:true, data:check })
         }
     
@@ -75,13 +75,13 @@ const createShortUrl = async function(req,res){
         else{
             let code = shortid.generate().toLowerCase()
             // let code = nanoid(6).toLowerCase()
-            console.log(code)
+            // console.log(code)
             let data = {longUrl : url, urlCode: code, shortUrl: `${protocol}://${hostName}/${code}` }
             // let data = {longUrl : url, urlCode: code, shortUrl: "http://localhost:3000/"+code }
             let result = await urlModel.create(data)
-            cache.set(code, data.longUrl)
+            // cache.set(code, data.longUrl)
             // cache.set(code, data.longUrl, 60*60*60*60)
-            await SET_ASYNC(`${result.urlCode}`, JSON.stringify(result.longUrl))
+            await SET_ASYNC(`${result.urlCode}`, result.longUrl)
             let my = {longUrl: result.longUrl, shortUrl: result.shortUrl, urlCode: result.urlCode}
             return res.status(201).send({status:true, data:my})
         }
@@ -92,7 +92,8 @@ const createShortUrl = async function(req,res){
 
 const getUrl = async function(req,res){
     try{
-        console.log(req.rawHeaders)
+        // console.log(req.rawHeaders)
+        let code = req.params.urlCode
         if(!req.params.urlCode) {
             return res.status(400).send({status :false, message: "Must send complete Url"})
         }
@@ -101,19 +102,20 @@ const getUrl = async function(req,res){
             console.log(req.params.urlCode)
             return res.status(400).send({status :false, message: "Not a valid Url"})
         }
-        let code = req.params.urlCode
-        if (cache.has(code)) { console.log("cache exist")}
-        else{   console.log("cache not exist")}
+        
+        // if (cache.has(code)) { console.log("cache exist")}
+        // else{   console.log("cache not exist")}
         // if (cache.has(code)) {
         //     console.log("cache exist")
         // }
         // else{
         //     console.log("cache not exist")
         // }
-        let cahcedUrl = await GET_ASYNC(`${req.params.urlCode}`)
-   if(cahcedUrl) {
-    return   res.status(302).redirect(cahcedUrl)
-   }
+        let cahcedUrl = await GET_ASYNC(`${code}`)
+        console.log(cahcedUrl)
+        if(cahcedUrl) {
+            return   res.status(302).redirect(cahcedUrl)
+        }
           
         // const checkCache = cache.get(code);
         // console.log(checkCache)
@@ -125,7 +127,7 @@ const getUrl = async function(req,res){
         if(!data){
             return res.status(404).send({status :false, message: "Not a valid Url"})                       //status code has to be changed
         }else{
-            await SET_ASYNC(`${data.urlCode}`, JSON.stringify(data.longUrl)) 
+            await SET_ASYNC(`${data.urlCode}`, data.longUrl) 
             res.status(302).redirect(data.longUrl);
         }
     }catch(error){
